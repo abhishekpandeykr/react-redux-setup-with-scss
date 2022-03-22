f96e910776bfe0be368a5cce01b3e07d6cec32dd
karma007 <abhishekpandey.kr@gmail.com>
2022-03-22 21:03:48 +0530

    webpack5 setup with scss and redux

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import "./style.scss";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
